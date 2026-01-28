9638d9b024ff464e2a82b57ab0c625b2b1b2b80e
gusrb9514 <gusrb9514@gmail.com>
2026-01-28 08:07:18 +0000

    Feat: Remove food images from menu recommendation

--- main.js
+++ main.js
@@ -5,34 +5,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const body = document.body;
 
     const lunchMenus = [
-        { name: "김치찌개", image: "https://static.photos/food/600/400" },
-        { name: "된장찌개", image: "https://static.photos/food/600/400" },
-        { name: "부대찌개", image: "https://static.photos/food/600/400" },
-        { name: "순두부찌개", image: "https://static.photos/food/600/400" },
-        { name: "제육볶음", image: "https://static.photos/food/600/400" },
-        { name: "오징어볶음", image: "https://static.photos/food/600/400" },
-        { name: "불고기", image: "https://static.photos/food/600/400" },
-        { name: "돈까스", image: "https://static.photos/food/600/400" },
-        { name: "비빔밥", image: "https://static.photos/food/600/400" },
-        { name: "김밥", image: "https://static.photos/food/600/400" },
-        { name: "라면", image: "https://static.photos/food/600/400" },
-        { name: "떡볶이", image: "https://static.photos/food/600/400" },
-        { name: "짜장면", image: "https://static.photos/food/600/400" },
-        { name: "짬뽕", image: "https://static.photos/food/600/400" },
-        { name: "탕수육", image: "https://static.photos/food/600/400" },
-        { name: "볶음밥", image: "https://static.photos/food/600/400" },
-        { name: "초밥", image: "https://static.photos/food/600/400" },
-        { name: "우동", image: "https://static.photos/food/600/400" },
-        { name: "라멘", image: "https://static.photos/food/600/400" },
-        { name: "카레", image: "https://static.photos/food/600/400" },
-        { name: "햄버거", image: "https://static.photos/food/600/400" },
-        { name: "피자", image: "https://static.photos/food/600/400" },
-        { name: "파스타", image: "https://static.photos/food/600/400" },
-        { name: "샌드위치", image: "https://static.photos/food/600/400" },
-        { name: "쌀국수", image: "https://static.photos/food/600/400" },
-        { name: "분짜", image: "https://static.photos/food/600/400" },
-        { name: "팟타이", image: "https://static.photos/food/600/400" },
-        { name: "나시고랭", image: "https://static.photos/food/600/400" }
+        "김치찌개", "된장찌개", "부대찌개", "순두부찌개",
+        "제육볶음", "오징어볶음", "불고기", "돈까스",
+        "비빔밥", "김밥", "라면", "떡볶이",
+        "짜장면", "짬뽕", "탕수육", "볶음밥",
+        "초밥", "우동", "라멘", "카레",
+        "햄버거", "피자", "파스타", "샌드위치",
+        "쌀국수", "분짜", "팟타이", "나시고랭"
     ];
 
     // Load theme from localStorage
@@ -69,8 +48,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const selectedMenu = lunchMenus[randomIndex];
         menuContainer.innerHTML = `
             <div class="menu-item">
-                <img src="${selectedMenu.image}" alt="${selectedMenu.name}" class="menu-image">
-                <span>${selectedMenu.name}</span>
+                <span>${selectedMenu}</span>
             </div>
         `;
     }
